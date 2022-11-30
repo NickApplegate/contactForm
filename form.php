356463fa567f1243f3c190cb441060ccabe056a4
Nick <119451630+NickApplegate@users.noreply.github.com>
2022-11-30 19:49:40 +0000

    added style to echo

--- form.php
+++ form.php
@@ -13,7 +13,7 @@
       <input type="submit" value="Submit">
     </form>
     <h1>Hello, world!</h1>
-    <?php echo $_POST['fName']; ?>
+    <?php echo "<p style='color:blue;'>".$_POST['fName']; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
